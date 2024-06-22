7121b60b9ab73ca035220bfdbb9182edda6eb6a7
Prashant kumar Chaturvedi <93850004+meprashantkumar@users.noreply.github.com>
2024-06-22 15:29:11 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import io from "socket.io-client";
 import { UserData } from "./UserContext";
 
-const EndPoint = "http://localhost:7000";
+const EndPoint = "https://mern-social-3e3m.onrender.com";
 
 const SocketContext = createContext();
 
