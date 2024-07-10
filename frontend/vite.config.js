acb5beb561d30e359a56ae5357a3d17188de3734
Prashant kumar Chaturvedi <93850004+meprashantkumar@users.noreply.github.com>
2024-07-10 10:16:47 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       "/api": {
