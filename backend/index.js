959d8b9ce1eaf623ed93abaaa4af5558237bdaf3
Prashant kumar Chaturvedi <93850004+meprashantkumar@users.noreply.github.com>
2024-07-19 13:13:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,83 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
+import cloudinary from "cloudinary";
+import cookieParser from "cookie-parser";
+import { Chat } from "./models/ChatModel.js";
+import { isAuth } from "./middlewares/isAuth.js";
+import { User } from "./models/userModel.js";
+import { app, server } from "./socket/socket.js";
 import path from "path";
 
 dotenv.config();
 
-const app = express();
+cloudinary.v2.config({
+  cloud_name: process.env.Cloudinary_Cloud_Name,
+  api_key: process.env.Cloudinary_Api,
+  api_secret: process.env.Cloudinary_Secret,
+});
 
-const port = process.env.PORT;
-
-// using middlewares
+//using middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/uploads", express.static("uploads"));
+const port = process.env.PORT;
+
+// to get all chats of user
+app.get("/api/messages/chats", isAuth, async (req, res) => {
+  try {
+    const chats = await Chat.find({
+      users: req.user._id,
+    }).populate({
+      path: "users",
+      select: "name profilePic",
+    });
+
+    chats.forEach((e) => {
+      e.users = e.users.filter(
+        (user) => user._id.toString() !== req.user._id.toString()
+      );
+    });
+
+    res.json(chats);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// to get all users
+app.get("/api/user/all", isAuth, async (req, res) => {
+  try {
+    const search = req.query.search || "";
+    const users = await User.find({
+      name: {
+        $regex: search,
+        $options: "i",
+      },
+      _id: { $ne: req.user._id },
+    }).select("-password");
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 // importing routes
-import userRoutes from "./routes/user.routes.js";
-import pinRoutes from "./routes/pin.routes.js";
-import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
-// using routes
-app.use("/api", userRoutes);
-app.use("/api", pinRoutes);
+//using routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/messages", messageRoutes);
 
 const __dirname = path.resolve();
 
@@ -32,7 +87,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
   connectDb();
 });
